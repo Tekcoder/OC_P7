ce0d2973e7c8238dd950b1908a7e6b6c076ba485
Soimuen11 <soimuen11@gmail.com>
2020-03-06 13:49:49 +0100

    Transition to VanillaJS
    As I had some trouble with react (not enough documentation for mapbox
    and leaflet), I decided to use mere VanillaJS along with html5 and css3.
    
    1. I removed everything related to ReactJS
    2. I re-organized the project and made it work with plain VanillaJS,
    html5 and css3.
    3. I managed to add my json data to the prject with an xhttp request.
    
    ERRORS :
    -> I still haven't found out why my background-img won't load.
    -> http-server errors:
        1. "GET /favicon.ico" Error (404): "Not found"
        2. "GET /Projects/OC_P7/css/img/Background-Graphics-For-Websites4.jpg" Error (404): "Not found"

---
+++ js/script.js
@@ -0,0 +1,46 @@
+"use strict"
+
+function getMap(){
+	let mymap = L.map('map').setView([51.505, -0.09], 13);
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		maxZoom: 18,
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1,
+		accessToken: 'pk.eyJ1Ijoic29pbXVlbjExIiwiYSI6ImNrNzM0Y2RvcjA4YnMzaG1uaWVyZGxidjcifQ.Ed1yaeFJ8-4ntjFgWIjLAg'
+	}).addTo(mymap);
+
+	let marker = L.marker([51.5, -0.09]).addTo(mymap);
+	let circle = L.circle([51.508, -0.11], {
+		color: 'red',
+		fillColor: '#f03',
+		fillOpacity: 0.5,
+		radius: 500
+	}).addTo(mymap);
+
+	let polygon = L.polygon([
+		[51.509, -0.08],
+		[51.503, -0.06],
+		[51.51, -0.047]
+	]).addTo(mymap);
+
+}
+
+function get_json_data(){
+	let data = null;
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			data = JSON.parse(xhttp.responseText)
+			console.log({data})
+		}
+	}
+	xhttp.open("GET", "./js/restaurants.json", true);
+	xhttp.send();
+}
+
+window.onload = function(event){
+	const map = getMap()
+	get_json_data()
+}
