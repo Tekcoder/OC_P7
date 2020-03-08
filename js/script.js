8b5e82b143a2198c630a06bb5c739ff592a2de8b
Soimuen11 <soimuen11@gmail.com>
2020-03-08 15:21:35 +0100

    made webpage great again
    1. fixed favicon error
    2. got restaurants on the map
    3. organized elements on page properly
    (with flexbox)
    4. fixed line break issue in header
    5. changed zoom and center of the map
    6. got json data outside of xhttp request and assigned it
    to "data" variable
    7. used getBound() method of leaflet to dtermine if restaurants are
    visible or not on the map

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 "use strict"
 
 function getMap(){
-	let mymap = L.map('map').setView([51.505, -0.09], 13);
+	let mymap = L.map('map').setView([48.8737815, 2.3501649], 10);
 	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		maxZoom: 18,
-		id: 'mapbox/streets-v11',
-		tileSize: 512,
-		zoomOffset: -1,
-		accessToken: 'pk.eyJ1Ijoic29pbXVlbjExIiwiYSI6ImNrNzM0Y2RvcjA4YnMzaG1uaWVyZGxidjcifQ.Ed1yaeFJ8-4ntjFgWIjLAg'
+		attribution : 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,\
+					   <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,\
+					   Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		maxZoom     : 18,
+		id          : 'mapbox/streets-v11',
+		tileSize    : 512,
+		zoomOffset  : -1,
+		accessToken : 'pk.eyJ1Ijoic29pbXVlbjExIiwiYSI6ImNrNzM0Y2RvcjA4YnMzaG1uaWVyZGxidjcifQ.Ed1yaeFJ8-4ntjFgWIjLAg'
 	}).addTo(mymap);
 	return mymap;
 }
@@ -35,15 +37,24 @@ function get_json_data(){
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			data = JSON.parse(xhttp.responseText)
-			console.log({data})
 		}
 	}
-	xhttp.open("GET", "./js/restaurants.json", true);
+	xhttp.open("GET", "./js/restaurants.json", false);
 	xhttp.send();
+	return data
 }
 
-window.onload = function(event){
+// window.onload = function(event){
 	const mymap = getMap()
-	customizeMap(mymap)
-	get_json_data()
-}
+	// customizeMap(mymap)
+	const data = get_json_data()
+	console.log(data)
+	let markers = new Array();
+	let i = 0;
+	for (let r of data) {
+		markers[i] = L.marker([r.lat, r.long]).addTo(mymap);
+		markers[i].bindPopup(r.name)
+		console.log(mymap.getBounds().contains(markers[i].getLatLng()) == true)
+		i = i + 1;
+	}
+// }
